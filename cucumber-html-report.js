b155d8c3c8fb1b3df8ddb6fe9c603fd54c94c268
shivangi0809 <72277106+shivangi0809@users.noreply.github.com>
2023-08-28 22:34:56 +0100

    Add files via upload

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -1,16 +1,15 @@
 const report = require("multiple-cucumber-html-reporter");
 report.generate({
   jsonDir: "jsonlogs", // ** Path of .json file **//
-  reportPath: "./reports/cucumber-htmlreport.html",
+  reportPath: "./reports/cucumber-report.html",
   metadata: {
     browser: {
       name: "chrome",
-      version: "115.0.5790.170",
+      version: "116",
     },
     device: "Local test machine",
     platform: {
-      name: "MAC",
-      version: "Ventura 13.5",
+      name: "WINDOW"
     },
   },
 });
