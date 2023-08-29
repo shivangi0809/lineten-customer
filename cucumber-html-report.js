8b385152deaef970b66fb26e809a081be7cd8921
shivangi0809 <72277106+shivangi0809@users.noreply.github.com>
2023-08-29 07:55:10 +0100

    Update cucumber-html-report.js

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const report = require("multiple-cucumber-html-reporter");
 report.generate({
   jsonDir: "jsonlogs", // ** Path of .json file **//
@@ -14,20 +13,4 @@ report.generate({
     },
   },
 });
-=======
-const report = require("multiple-cucumber-html-reporter");
-report.generate({
-  jsonDir: "jsonlogs", // ** Path of .json file **//
-  reportPath: "./reports/cucumber-report.html",
-  metadata: {
-    browser: {
-      name: "chrome",
-      version: "116",
-    },
-    device: "Local test machine",
-    platform: {
-      name: "WINDOW"
-    },
-  },
-});
->>>>>>> b155d8c3c8fb1b3df8ddb6fe9c603fd54c94c268
+
