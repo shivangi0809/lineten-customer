bac75f7074cf67288ac9273a8d40ed9699cdcb41
shivangi0809 <shivi.monal@gmail.com>
2023-09-18 19:52:07 +0100

    radme updated

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 const report = require("multiple-cucumber-html-reporter");
 report.generate({
-  jsonDir: "jsonlogs", // ** Path of .json file **//
-  reportPath: "./reports/cucumber-report.html",
+  reportPath: "./cucumber-report.html",
   metadata: {
     browser: {
       name: "chrome",
@@ -14,20 +12,3 @@ report.generate({
     },
   },
 });
-=======
-const report = require("multiple-cucumber-html-reporter");
-report.generate({
-  jsonDir: "jsonlogs", // ** Path of .json file **//
-  reportPath: "./reports/cucumber-report.html",
-  metadata: {
-    browser: {
-      name: "chrome",
-      version: "116",
-    },
-    device: "Local test machine",
-    platform: {
-      name: "WINDOW"
-    },
-  },
-});
->>>>>>> b155d8c3c8fb1b3df8ddb6fe9c603fd54c94c268
